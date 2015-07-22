319ab89f2d8a1ac1da7dc65f2c3512e243cee02b
unknown <test@L-MAA-25004136.paypalcorp.com>
2015-07-22 15:21:04 +0530

    Template

---
+++ odometer.c
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+#define MIN 1
+#define MAX 9
+
+class Odometer {
+	
+	public:
+		int length;
+		Odometer(int);
+		bool isValid(int number[])
+		int[] nextNumber(int number[]);
+		int[] previousNumber(int number[]);
+}
+
+Odometer::Odometer(int lenght) {
+	this->length = length;
+}
